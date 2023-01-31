52500396adac45d5653a3ad1459237ec84b8208a
Samuels04 <samuelszgz004@gmail.com>
2023-01-31 16:52:57 +0100

    Just modified a bit the toString Method of the CommissionEmployee class

--- Programming Methodology/310123/ProjectWithTwoClasses/src/es/employee/CommissionEmployee.java
+++ Programming Methodology/310123/ProjectWithTwoClasses/src/es/employee/CommissionEmployee.java
@@ -74,30 +74,11 @@ public class CommissionEmployee extends Object {
    // calculate commission employee's pay
    public double earnings() {
       return commissionRate * grossSales;
-   } // end method earnings
+   } 
 
    // return String representation of CommissionEmployee object
-   @Override // indicates that this method overrides a superclass method
+   @Override 
    public String toString() {
-      return String.format("%s: %s %s\n%s: %s\n%s: %.2f\n%s: %.2f",
-            "commission employee", firstName, lastName,
-            "social security number", socialSecurityNumber,
-            "gross sales", grossSales,
-            "commission rate", commissionRate);
-   } // end method toString
-} // end class CommissionEmployee
-
-/**************************************************************************
- * (C) Copyright 1992-2010 by Deitel & Associates, Inc. and *
- * Pearson Education, Inc. All Rights Reserved. *
- * *
- * DISCLAIMER: The authors and publisher of this book have used their *
- * best efforts in preparing the book. These efforts include the *
- * development, research, and testing of the theories and programs *
- * to determine their effectiveness. The authors and publisher make *
- * no warranty of any kind, expressed or implied, with regard to these *
- * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or *
- * consequential damages in connection with, or arising out of, the *
- * furnishing, performance, or use of these programs. *
- *************************************************************************/
+      return String.format("Commission Employee: %s %s\nSocial Security Number: %s\nGross sales: %.2f\nCommission rate: %.2f", firstName, lastName, socialSecurityNumber, grossSales, commissionRate);
+   }
+}
