66ae4ded449315ab8a9af89a16ce38cee334fe03
Samuel <samuelszgz004@gmail.com>
2023-02-25 17:47:53 +0100

    Created Assembly file
    Created this assembly file on Xcode to practice Computer Science Fundamentals

---
+++ CNF/Assembly_practice/Assembly_practice/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Assembly_practice
+//
+//  Created by Samuel Sánchez González on 25/02/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ CNF/Assembly_practice/practice.s
@@ -0,0 +1,10 @@
+//
+//  practice.s
+//  Assembly_practice
+//
+//  Created by Samuel Sánchez González on 25/02/2023.
+//
+
+;Exercise_1
+
+
