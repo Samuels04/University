73a9014ce58647620bcac88b6929e5c360f8e2be
Samuel <samuelszgz004@gmail.com>
2024-02-09 11:07:41 +0100

    Started on project

---
+++ 2nd Year/2nd Semester/Human Machine Interaction/JavaApplication1/src/javaapplication1/Element.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Samuel
+ */
+public class Element {
+    private String name;
+    private String symbol;
+    private int atomicNumber;
+    private int massNumber;
+    private boolean favourite;
+    private String group;
+    
+    
+    
+    public Element(String name, int atomicNumber, int massNumber, String symbol) {
+    
+        this.name = name;
+        this.symbol = symbol;
+        this.atomicNumber = atomicNumber;
+        this.massNumber = massNumber;
+        this.favourite = false;
+    
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    public int getAtomicNumber(){
+        return this.atomicNumber;
+    }
+    public int getMassNumber(){
+        return this.massNumber;
+    }
+    public void favourite(){
+        favourite = true;
+    }
+    public void unfavourite(){
+        favourite = false;
+    }
+}
