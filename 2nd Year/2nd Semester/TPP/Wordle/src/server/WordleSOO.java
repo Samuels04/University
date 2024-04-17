3f6b03c83fd2772c0b162f715262fcbf4e1c81c1
Samuel <uo293747@uniovi.es>
2024-04-17 17:56:41 +0200

    1st lab

--- 2nd Year/2nd Semester/TPP/Wordle/src/server/WordleSOO.java
+++ 2nd Year/2nd Semester/TPP/Wordle/src/server/WordleSOO.java
@@ -18,12 +18,19 @@ import commun.*;
 public class WordleSOO implements IWordle {
 	// Constants
 	static final int MAX_ATTEMPTS = 5;
+	
+	
+	int state = 0;
 
+	
+	int numberAttempts = 0;
 	// Area of data
 
 	// List of possible words to guess
 	// All words will be of size IWordle.WORD_SIZE
 	List<String> wordsGuess;
+	
+	String secretWord;
 
 	// Constructor
 	public WordleSOO(int id) throws DictionaryError {
@@ -69,9 +76,67 @@ public class WordleSOO implements IWordle {
 		}
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public String obtainDifferences(String word) throws PA_Exception, WrongLength, WrongFormat {
-
+		
+		String wordUppercase = word.toUpperCase();
+		
+		if(word.length() != WORD_SIZE)
+			throw new WrongLength(String.format("The guess must be %d letters long", WORD_SIZE));
+		
+		StringBuilder result = new StringBuilder();
+		
+		Map<Character, Integer> occurences = new HashMap<Character, Integer>();
+		
+		Map<Character, Integer> occurencesWell = new HashMap<Character, Integer>();
+		
+		for(int i = 0; i <= word.length(); i++) {
+			
+			char currentChar = wordUppercase.charAt(i);
+			
+			if(occurences.containsKey(currentChar))
+				occurences.put(currentChar, occurences.get(currentChar) + 1);
+			else
+				occurences.put(currentChar, 1);
+			
+			if(currentChar == secretWord.charAt(i)) 
+				if(occurencesWell.containsKey(currentChar))
+					occurencesWell.put(currentChar, occurencesWell.get(currentChar) + 1);
+				else
+					occurencesWell.put(currentChar, 1);
+		}
+		
+		for(int i = 0; i <= word.length(); i++) {
+			
+			char currentChar = wordUppercase.charAt(i);
+			
+			if(!occurences.containsKey(currentChar) || occurences.get(currentChar) == 0 || occurencesWell.get(currentChar) == 0)
+				
+				result.append('X');
+			
+			else if(occurences.containsKey(currentChar) && !occurencesWell.containsKey(currentChar)) {
+				
+				result.append('C');
+				
+				occurences.put(currentChar, occurences.get(currentChar) - 1);
+				
+			} else if(occurencesWell.containsKey(currentChar) && currentChar != wordUppercase.charAt(i)) {  
+				
+				result.append('C');
+				occurences.put(currentChar, occurences.get(currentChar) - 1);
+		
+			} else if(occurencesWell.containsKey(currentChar) && currentChar == wordUppercase.charAt(i)) {  
+				
+				result.append('=');
+				occurences.put(currentChar, occurences.get(currentChar) - 1);
+				occurencesWell.put(currentChar, occurencesWell.get(currentChar) - 1);
+			}
+			
+		}
+		
+		return result.toString();
+		
 		// THIS OPERATION IS NOT COMPLETE.
 
 		// HELP (STEPS) TO FOLLOW TO DETECT DIFFERENCES CORRECTLY
@@ -107,5 +172,40 @@ public class WordleSOO implements IWordle {
 
 	private boolean validWord(String word) {
 		return word.matches("[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ]+");
+	}
+
+	@Override
+	public void restartGame() throws PA_Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void newWord() throws PA_Exception, WrongLength {
+		
+		if(state != 0)
+			throw new PA_Exception("Cannot be called");
+		
+		if(numberAttempts == MAX_ATTEMPTS)
+			throw new PA_Exception("Already reached max number of attempts");
+		
+		numberAttempts++;
+		Random rand = new Random();
+		
+		int secretWordIndex = rand.nextInt(0, this.wordsGuess.size());
+		this.secretWord = this.wordsGuess.get(secretWordIndex);
+		
+	}
+
+	@Override
+	public int endedGame(String word) throws PA_Exception, WrongLength, WrongFormat {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String requestWord() throws PA_Exception {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
