7946e263000024150c4bd53da4d3793869ef3253
Samuel Sanchez Gonzalez - Laptop <uo293747@uniovi.es>
2025-03-21 20:24:19 +0100

    Continued working on Lab1 AIKE, changed Graph implementation and coded Dijkstra's for 1.A

--- 3rd Year/AIKE/Lab 1/Lab 1/src/App.java
+++ 3rd Year/AIKE/Lab 1/Lab 1/src/App.java
@@ -1,61 +1,121 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Time;
 import java.util.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        final String file = "C:\\Users\\samue\\Code\\University\\3rd Year\\AIKE\\Lab 1\\Lab 1\\src\\connection_graph.csv";
-        Graph transportGraph;
-        List<Segment> segments = new ArrayList<>();
-
 
     }
 
-    static void readFile(String file, Graph transportGraph) {
 
-        try {
-            // Create an object of filereader
-            // class with CSV file as a parameter.
-            FileReader filereader = new FileReader(file);
-            BufferedReader br = new BufferedReader(filereader);
-            String line;
-            br.readLine();
-            while ((line = br.readLine()) != null) {
-                //Get the next line in the CSV file
-                String[] currentRow = line.split(",");
+    static void Task1A(String source, String target, int startTime) throws IOException {
+        final String file = "C:\\Users\\samue\\Code\\University\\3rd Year\\AIKE\\Lab 1\\Lab 1\\src\\connection_graph.csv";
+        Graph<String, Integer> graph = new Graph<>();
+        // Create an object of filereader
+        // class with CSV file as a parameter.
+        FileReader filereader = new FileReader(file);
+        BufferedReader br = new BufferedReader(filereader);
+        String line;
+        br.readLine();
+        while ((line = br.readLine()) != null) {
+            //Get the next line in the CSV file
+            String[] currentRow = line.split(",");
 
-                //Extract all the fields in the line
-                int id = Integer.parseInt(currentRow[0]);
-                String company = currentRow[1];
-                String lineT = currentRow[2];
-                Time departure = Time.valueOf(currentRow[3]);
-                Time arrival = Time.valueOf(currentRow[4]);
-                String start = currentRow[5];
-                String end = currentRow[6];
-                double startLat = Double.parseDouble(currentRow[7]);
-                double startLon = Double.parseDouble(currentRow[8]);
-                double endLat = Double.parseDouble(currentRow[9]);
-                double endLon = Double.parseDouble(currentRow[10]);
+            //Extract all the fields in the line
+            int id = Integer.parseInt(currentRow[0]);
+            String company = currentRow[1];
+            String lineT = currentRow[2];
+            int departure = Integer.parseInt(currentRow[3]);
+            int arrival = Integer.parseInt(currentRow[4]);
+            String start = currentRow[5];
+            String end = currentRow[6];
+            double startLat = Double.parseDouble(currentRow[7]);
+            double startLon = Double.parseDouble(currentRow[8]);
+            double endLat = Double.parseDouble(currentRow[9]);
+            double endLon = Double.parseDouble(currentRow[10]);
+            if(departure > startTime){
+                //Only add those segments that departure after the start time of the journey
+                graph.addVertex(start);
+                graph.addVertex(end);
+                graph.addEdge(start, end, arrival-departure);
+            } else
+                continue;
 
-                //Create a new segment with the fields
-                Segment segment = new Segment(id, company, lineT, departure, arrival, start, end, startLat, startLon, endLat, endLon);
+        }
+        //Create all necessary structures for the algorithm
+        Map<String, Integer> mDistances = new TreeMap<String, Integer>();  //Distances from start vertex to another one
+        Map<String, String> mPredecessors = new TreeMap<String, String>();
+        Set<String> visited = new HashSet<String>(graph.getVertexCount()*2); //Twice the number of vertices to reduce the number of collisions and increase the efficiency
+        Set<String> unvisited = new HashSet<String>(graph.getAllVertices());
 
-                //Add the segment and the stops to the graph
-                transportGraph.addVertex(segment.getStartStop());
-                transportGraph.addVertex(segment.getEndStop());
-                if(transportGraph instanceof GraphTransportDistance)
-                    ((GraphTransportDistance) transportGraph)
-                            .addEdge(segment, segment.getLength());
+        for(String v : unvisited){
+            if(source.equals(v))
+                mDistances.put(v, 0);
+            else {
+                if (!graph.hasEdge(source, v))
+                    mDistances.put(v, Integer.MAX_VALUE);
+                else {
+                    mDistances.put(v, graph.edgeWeight(source, v));
+                    mPredecessors.put(v, source);
+                }
             }
+        }
+        visited.add(source);
+        unvisited.remove(source);
 
-            } catch (Exception e) {
-            e.printStackTrace();
+
+        while(!unvisited.isEmpty()){
+            String next = minDistDijkstra(mDistances, visited);
+            visited.add(next);
+            unvisited.remove(next);
+
+            if (next == null)
+                break; //Possible isolated vertices
+
+            Set<String> adjacents = graph.adjacentsTo(next);
+            for (String w: adjacents) {
+                int dist_w = mDistances.get(w);
+                int dist_next_w = mDistances.get(next) + graph.edgeWeight(next, w);
+                if (dist_w > dist_next_w) {
+                    mDistances.put(w, dist_next_w);
+                    mPredecessors.put(w, next);
+                }
             }
         }
 
-        static void Task1(){}
-        static void Task2(){}
-        static void Task3(){}
-        static void Task4(){}
+        /*Here finishes Dijkstra's algorithm, now I must find the path
+        */
+        List<String> path = new ArrayList<>();
+        String current = target;
+        while (current != null && !current.equals(source)) {
+            path.add(current);
+            current = mPredecessors.get(current);
+        }
+        path.add(source);
+        Collections.reverse(path);
+
+
+
+    }
+    //Method for calculating the next vertex with the minimum distance for Dijkstra's algorithm
+    static String minDistDijkstra(Map<String, Integer> distances, Set<String> visited) {
+        int minWeight = Integer.MAX_VALUE;
+        String selected = null;
+
+        for (Map.Entry<String, Integer> entry : distances.entrySet()) {
+            int weight = entry.getValue();
+            String v = entry.getKey();
+            if (!visited.contains(v) && weight < minWeight ) {
+                minWeight = weight;
+                selected = v;
+            }
+        }
+        return selected;
+    }
+    static void Task2B(){}
+    static void Task3C(){}
+    static void Task4D(){}
 }
--- 3rd Year/AIKE/Lab 1/Lab 1/src/Graph.java
+++ 3rd Year/AIKE/Lab 1/Lab 1/src/Graph.java
@@ -1,14 +1,159 @@
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
-public interface Graph {
-    Set<String> adjacentsTo(String v);
-    Set<String> getAllVertices();
-    int getVertexCount();
-    public int getEdgeCount();
-    boolean addVertex(String v);
-    boolean hasVertex(String v);
-    boolean removeVertex(String v);
-    boolean hasEdge(Segment edge);
-    boolean removeEdge(Segment edge);
+public class Graph<T, U>{
+	private Map<T, TreeMap<T, U>> mGraph;
+	private int mVertexCount;
+	private int mEdgeCount;
+
+	public Set<T> adjacentsTo(T v)
+	{
+		if (hasVertex(v))
+			return mGraph.get(v).keySet();
+		else
+			return new HashSet<>();
+	}
+
+	public Set<T> getAllVertices()
+	{
+		return mGraph.keySet();
+	}
+
+	public int getVertexCount()
+	{
+		return mVertexCount;
+	}
+
+	public int getEdgeCount()
+	{
+		return mEdgeCount;
+	}
+
+	public Graph()
+	{
+		mGraph = new TreeMap<T, TreeMap<T,U>> ();
+		mVertexCount=0;
+		mEdgeCount=0;
+	}
+
+	public Graph(Graph<T, U> g)
+	{
+		mGraph = new TreeMap<T, TreeMap<T, U>> (g.mGraph);
+		mVertexCount = g.mVertexCount;
+		mEdgeCount = g.mEdgeCount;
+	}
+
+	public boolean addVertex(T v)
+	{
+		if (hasVertex(v))
+			return false;   //Can't repeat vertices
+
+		mVertexCount++;
+
+		//Empty adjacency list
+		TreeMap<T, U> aux = new TreeMap<T, U>();
+		mGraph.put(v, aux);  //Vertex added to the graph
+
+		return true;
+	}
+
+	public boolean hasVertex(T v)
+	{
+		return mGraph.containsKey(v);
+	}
+
+	public boolean removeVertex(T v)
+	{
+		//Not in the graph
+		if (!mGraph.containsKey(v))
+			return false;
+
+		//We have to remove first all the occurrences in the adjacency lists
+		for (Map<T, U> adjList : mGraph.values())
+			adjList.remove(v);
+
+		//We finally remove it from the graph
+		mGraph.remove(v);
+		mVertexCount--;
+
+		return true;
+	}
+
+	public int InDegree(T v)
+	{
+		if (!mGraph.containsKey(v))
+			return 0;
+
+		int counter = 0;
+		//Look for the vertex in all the adjacency lists
+		for (Map<T, U> adjList : mGraph.values())
+			if (adjList.containsKey(v))
+				counter++;
+
+		return counter;
+	}
+
+	public int OutDegree(T v)
+	{
+		if (!mGraph.containsKey(v))
+			return 0;
+
+		Map<T, U> adjList = mGraph.get(v);
+
+		return adjList.size();
+	}
+
+	public boolean hasEdge(T from, T to)
+	{
+		/*if (from.equals(to))
+			 return true;*/
+
+		if (!mGraph.containsKey(from) || !mGraph.containsKey(to))
+			return false;
+
+		Map<T, U> adjList = mGraph.get(from);
+
+		return (adjList.containsKey(to));
+	}
+
+	public boolean addEdge(T from, T to, U weight)
+	{
+		if (hasEdge(from,to))
+			return false;
+
+		mEdgeCount++;
+
+		addVertex(from);
+		addVertex(to);
+
+		Map<T, U> adjList = mGraph.get(from);
+		adjList.put(to, weight);
+
+		return true;
+	}
+
+	public boolean removeEdge(T from, T to)
+	{
+		if (!hasEdge(from, to))
+			return false;
+
+		Map<T, U> adjList = mGraph.get(from);
+		adjList.remove(to);
+
+		mEdgeCount--;
+
+		return true;
+	}
+
+	public U edgeWeight(T from, T to)
+	{
+		 /*if (from.equals(to))
+			 return 0.0;*/
+
+		if (!hasEdge(from, to))
+			return null;
+
+		Map<T, U> adjList = mGraph.get(from);
+		return adjList.get(to);
+	}
+
 }
