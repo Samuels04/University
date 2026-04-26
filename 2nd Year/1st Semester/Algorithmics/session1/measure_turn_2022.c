7f4fee28a6663ec3932d4f5fa7b4675ff6270c8a
Samuel Sánchez González <samuelszgz004@gmail.com>
2026-04-26 13:46:36 -0400

    Formatting of sime files

--- 2nd Year/1st Semester/Algorithmics/session1/measure_turn_2022.c
+++ 2nd Year/1st Semester/Algorithmics/session1/measure_turn_2022.c
@@ -11,107 +11,88 @@
 //
 // ------------------------------------------------------------------------------------------
 //
-// The library time.h provides a function to measure the time. It is named as clock.
+// The library time.h provides a function to measure the time. It is named as
+// clock.
 //
 // clock_t clock(void);
 //
-// This function returns the consumption time spends by the processor (in tics from the clock)
-// until the call to the function clock is made. The time in seconds is determined doing the
-// the division of the value returned by the function clock and the value from the
-// macro CLOCKS_PER_SEC.
+// This function returns the consumption time spends by the processor (in tics
+// from the clock) until the call to the function clock is made. The time in
+// seconds is determined doing the the division of the value returned by the
+// function clock and the value from the macro CLOCKS_PER_SEC.
 //
-// The library stdlib.h provides the function rand which chooses a number randomly. If random number
-// inside a specific range is required, for example between 0 and 1000, we will use
-// the operator modulo (%) as follow:
+// The library stdlib.h provides the function rand which chooses a number
+// randomly. If random number inside a specific range is required, for example
+// between 0 and 1000, we will use the operator modulo (%) as follow:
 //
 // rand()%1000
 //
 // ------------------------------------------------------------------------------------------
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
+#define NUM_SIZES 12
 
-# define NUM_SIZES 12
-
-
-# define REPETITIONS 100
-
+#define REPETITIONS 100
 
 int fillOut(int *, int);
 int turn(int *, int);
 
+int main() {
+  int i, j;
+  clock_t time_start, time_final;
 
-int main()
-{
- int i,j;
- clock_t time_start, time_final;
+  int sizes[NUM_SIZES] = {1000, 2000, 3000, 4000,  5000,  6000,
+                          7000, 8000, 9000, 10000, 11000, 12000};
 
+  printf("\n\nTURN A VECTOR\n\n");
+  printf("Consumption time:\n\n\n");
+  printf("\t\tSize\t\tTime\n\n");
+  printf("\t\t-----\t\t------\n\n");
 
- int sizes[NUM_SIZES]={1000,2000,3000,4000,5000,6000,7000,8000,9000,10000, 11000,12000};
+  for (i = 0; i < NUM_SIZES; i++) {
 
+    int *V = (int *)malloc(sizes[i] * sizeof(int));
 
- printf("\n\nTURN A VECTOR\n\n");
- printf("Consumption time:\n\n\n");
- printf("\t\tSize\t\tTime\n\n");
- printf("\t\t-----\t\t------\n\n");
+    fillOut(V, sizes[i]);
 
+    time_start = clock();
 
+    for (j = 1; j <= REPETITIONS; j++)
 
- for ( i = 0; i < NUM_SIZES; i++)
-     {
-     
-     int *V = (int*)malloc(sizes[i]*sizeof(int));
+      turn(V, sizes[i]);
 
-     
-     fillOut(V,sizes[i]);
+    time_final = clock();
 
-     
-     time_start = clock();
+    // Release the space allocated for the vector
+    free(V);
 
-    
-     for (j=1; j <= REPETITIONS; j++)
-       
-         turn(V, sizes[i]);
-
-     
-     time_final=clock();
-
-     // Release the space allocated for the vector
-     free(V);
-
-     
-     printf("\t\t%d\t\t%f\n", sizes[i],(time_final-time_start) /(double)CLOCKS_PER_SEC / REPETITIONS);
-     }
+    printf("\t\t%d\t\t%f\n", sizes[i],
+           (time_final - time_start) / (double)CLOCKS_PER_SEC / REPETITIONS);
+  }
   return 0;
 }
 
-
-int fillOut(int *V, int n)
-{
-    int i;
-    for (i=1;i<=n;i++)
-        V[i-1]=rand()%1000;
-    return 0;
+int fillOut(int *V, int n) {
+  int i;
+  for (i = 1; i <= n; i++)
+    V[i - 1] = rand() % 1000;
+  return 0;
 }
 
+int turn(int *V, int n) {
+  int i, j, aux;
 
-int turn(int *V,int n)
-{
-    int i, j, aux;
+  for (i = 1; i < n - 2; i++) {
 
-    for (i = 1; i < n -2; i++){
+    for (j = 0; j <= n - i; j++) {
 
-        for (j = 0; j <= n - i; j++) {
-
-            aux = V[j-1];
-            V[j-1] = V[j];
-            V[j] = aux;
-
-        }
-        
+      aux = V[j - 1];
+      V[j - 1] = V[j];
+      V[j] = aux;
     }
-    return *V;
+  }
+  return *V;
 }
--- 2nd Year/1st Semester/Algorithmics/session2/prototypes.c
+++ 2nd Year/1st Semester/Algorithmics/session2/prototypes.c
@@ -1,12 +1,12 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdbool.h>
 
-# define NUM_SIZES 10
-# define REPITERA 100000
-# define REPRECUR 10000
-# define REPSEARCH 1000
+#define NUM_SIZES 10
+#define REPITERA 100000
+#define REPRECUR 10000
+#define REPSEARCH 1000
 
 // Functions prototypes
 int fillOut(int **, int, int);
@@ -15,29 +15,26 @@ int AddRecur(int **, int);
 bool Search(int **, int, int);
 
 // Main program
-int main()
-{
-  int i,j;
+int main() {
+  int i, j;
 
   clock_t time_ini1, time_ini2, time_fin1, time_fin2;
 
-  int sizes[NUM_SIZES]={1000,2000,3000,4000,5000,6000,7000,8000,9000,10000};
+  int sizes[NUM_SIZES] = {1000, 2000, 3000, 4000, 5000,
+                          6000, 7000, 8000, 9000, 10000};
 
-  int **Matrix = NULL, AddIterResult = 0, AddRecurResult = 0; 
-
+  int **Matrix = NULL, AddIterResult = 0, AddRecurResult = 0;
 
   printf("\n\n\t\tLAB SESSION 2 - ANALYSIS OF ALGORITHMS  \n\n");
 
   printf("Time used:\n\n\n");
 
+  // STUDENT: COMMENT/UNCOMMENT THE NEXT printf ACCORDING TO THE MEASURES YOU
+  // NEED TO TAKE
 
-  // STUDENT: COMMENT/UNCOMMENT THE NEXT printf ACCORDING TO THE MEASURES YOU NEED TO TAKE
+  // printf("\t\tsizes\t\tTimeBest case\tTime Worst case\n");
 
-
-  //printf("\t\tsizes\t\tTimeBest case\tTime Worst case\n");
-
-  //printf("\t\t-----\t\t----------------\t----------------\n");
-
+  // printf("\t\t-----\t\t----------------\t----------------\n");
 
   printf("\t\tsizes\t\tIterative Time\tRecursive Time\n");
 
@@ -45,19 +42,16 @@ int main()
 
   for (i = 0; i < NUM_SIZES; i++) {
 
-    Matrix = (int**) malloc (sizes[i] * sizeof(int*));
+    Matrix = (int **)malloc(sizes[i] * sizeof(int *));
 
-    for(int j = 0; j < sizes[i]; j++){
+    for (int j = 0; j < sizes[i]; j++) {
 
-      Matrix[j] = (int*) malloc (sizes[i] * sizeof(int*));
-
+      Matrix[j] = (int *)malloc(sizes[i] * sizeof(int *));
     }
-
 
     int valueToFill = 2;
 
-    fillOut(Matrix,sizes[i], valueToFill);
-
+    fillOut(Matrix, sizes[i], valueToFill);
 
     // SNIPPET OF CODE TO ANALYSE THE SEARCH IN THE MATRIX
 
@@ -78,13 +72,14 @@ int main()
     time_fin2 = clock();
 
 
-    printf("\t\t%d\t\t%f", sizes[i],(time_fin1-time_ini1) /(double)CLOCKS_PER_SEC / REPSEARCH);
-    printf("\t\t%f\n",              (time_fin2-time_ini2) /(double)CLOCKS_PER_SEC / REPSEARCH);
+    printf("\t\t%d\t\t%f", sizes[i],(time_fin1-time_ini1)
+    /(double)CLOCKS_PER_SEC / REPSEARCH); printf("\t\t%f\n",
+    (time_fin2-time_ini2) /(double)CLOCKS_PER_SEC / REPSEARCH);
 
     */
 
-    // SNIPPET OF CODE TO ANALYSE THE ADDITION OF THE ELEMENTS IN THE DIAGONAL OF THE MATRIX
-    
+    // SNIPPET OF CODE TO ANALYSE THE ADDITION OF THE ELEMENTS IN THE DIAGONAL
+    // OF THE MATRIX
 
     time_ini1 = clock();
 
@@ -93,47 +88,45 @@ int main()
 
     time_fin1 = clock();
 
-
     time_ini2 = clock();
 
-    for (j = 1 ;j <= REPRECUR; j++)
+    for (j = 1; j <= REPRECUR; j++)
       AddRecurResult = AddRecur(Matrix, sizes[i]);
 
     time_fin2 = clock();
 
+    if (AddRecurResult != AddIterResult) {
+      printf("Inconsistency in the addition\n\n");
+      return -1;
+    }
 
-    if (AddRecurResult != AddIterResult) { printf("Inconsistency in the addition\n\n"); return -1; }
+    printf("\t\t%d\t\t%f", sizes[i],
+           (time_fin1 - time_ini1) / (double)CLOCKS_PER_SEC / REPITERA);
 
+    printf("\t\t%f\n",
+           (time_fin2 - time_ini2) / (double)CLOCKS_PER_SEC / REPRECUR);
 
-    printf("\t\t%d\t\t%f", sizes[i],(time_fin1-time_ini1) /(double)CLOCKS_PER_SEC / REPITERA);
-   
-    printf("\t\t%f\n",              (time_fin2-time_ini2) /(double)CLOCKS_PER_SEC / REPRECUR);
-
-  
-    for(int j = 0; j < sizes[i]; j++)
+    for (int j = 0; j < sizes[i]; j++)
       free(Matrix[j]);
-    
-    free(Matrix);
 
+    free(Matrix);
   }
   return 0;
 }
 
-
 // Functions definition
-int fillOut(int **M, int n, int m){
+int fillOut(int **M, int n, int m) {
 
-  for(int i = 0; i < n; i++)
+  for (int i = 0; i < n; i++)
 
-    for(int j = 0; j < n; j++)
+    for (int j = 0; j < n; j++)
 
       M[i][j] = m;
-
 }
 
-bool Search(int **M, int n, int x){
+bool Search(int **M, int n, int x) {
 
-  int i,j;
+  int i, j;
 
   bool found;
 
@@ -141,49 +134,44 @@ bool Search(int **M, int n, int x){
 
   found = false;
 
-  while(i < n && !found){
+  while (i < n && !found) {
 
     i++;
 
     j = 0;
 
-    while(j < n && !found){
+    while (j < n && !found) {
 
       j++;
 
       if (M[i - 1][j - 1] == x)
         found = true;
-      
     }
-
   }
 
   return found;
-
 }
 
-int AddIter(int**M, int n){
+int AddIter(int **M, int n) {
 
   int i, sum;
 
   sum = 0;
 
-  for(int i = 0; i < n; i++)
+  for (int i = 0; i < n; i++)
 
     sum += M[i][i];
 
   return sum;
-
 }
 
-int AddRecur(int **M, int n){
+int AddRecur(int **M, int n) {
 
-  if(n == 1)
+  if (n == 1)
 
     return M[0][0];
 
-  else 
+  else
 
     return M[n - 1][n - 1] + AddRecur(M, n - 1);
-
 }
